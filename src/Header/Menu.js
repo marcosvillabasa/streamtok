8eed013a0b4ee78276f39e33aecf8ff3342add4c
Christopher Rieder <christopher.rieder@gmail.com>
2020-06-27 20:53:11 -0300

    refactor: move useMenuSwipe to its own file

--- src/Header/Menu.js
+++ src/Header/Menu.js
@@ -7,8 +7,7 @@ import { StyledNavLink } from "../Components/NavLink"
 // import { SearchBox2 } from "../Components/SearchBox2"
 import fixedLogo from "../assets/logostreamtokfixed.svg"
 import { NavLink } from "react-router-dom"
-import { useMediaQuery, useTheme } from "@material-ui/core"
-import { checkSwipeClass } from "../Utils/checkSwipeClass"
+import { useMenuSwipe } from "../Utils/useMenuSwipe"
 
 const HeaderContainer = styled.header`
   padding: 16px;
@@ -124,57 +123,7 @@ const Wrapper = styled.div`
 `
 
 export default function Menu(props) {
-  const [open, setOpen] = React.useState(false)
-  const theme = useTheme()
-  const isMobile = useMediaQuery(theme.breakpoints.down("xs"))
-
-  const rootRef = React.useRef(document.querySelector("#root"))
-  const bodyRef = React.useRef(document.querySelector("body"))
-
-  const closeMenu = React.useCallback(() => {
-    setOpen(false)
-    bodyRef.current.style = "overflow: overlay;"
-  }, [])
-
-  React.useEffect(() => {
-    if (isMobile) {
-      let start_scroll = null
-      const onTouchStart = (event) => {
-        if (checkSwipeClass(event)) {
-          start_scroll = null
-        } else {
-          start_scroll = {
-            x: event.changedTouches[0].clientX,
-            y: event.changedTouches[0].clientY,
-          }
-        }
-      }
-      const onTouchMove = (event) => {
-        if (start_scroll && event.changedTouches?.length) {
-          if (Math.abs(start_scroll.y - event.changedTouches[0].clientY > 50)) {
-            start_scroll = null
-            return
-          }
-          if (event.changedTouches[0].clientX - start_scroll.x > 100) {
-            bodyRef.current.style = "overflow: hidden;"
-            setOpen(true)
-          } else if (start_scroll.x - event.changedTouches[0].clientX > 100) {
-            bodyRef.current.style = "overflow: overlay;"
-            setOpen(false)
-          }
-        }
-      }
-
-      const root = rootRef.current
-      root.addEventListener("touchstart", onTouchStart)
-      root.addEventListener("touchmove", onTouchMove)
-
-      return () => {
-        root.removeEventListener("touchstart", onTouchStart)
-        root.removeEventListener("touchmove", onTouchMove)
-      }
-    }
-  }, [isMobile])
+  const { open, closeMenu } = useMenuSwipe()
 
   return (
     <Wrapper>
---
+++ src/Utils/useMenuSwipe.js
@@ -0,0 +1,59 @@
+import React from "react"
+import { useMediaQuery, useTheme } from "@material-ui/core"
+import { checkSwipeClass } from "../Utils/checkSwipeClass"
+
+export function useMenuSwipe() {
+  const [open, setOpen] = React.useState(false)
+  const theme = useTheme()
+  const isMobile = useMediaQuery(theme.breakpoints.down("xs"))
+
+  const rootRef = React.useRef(document.querySelector("#root"))
+  const bodyRef = React.useRef(document.querySelector("body"))
+
+  const closeMenu = React.useCallback(() => {
+    setOpen(false)
+    bodyRef.current.style = "overflow: overlay;"
+  }, [])
+
+  React.useEffect(() => {
+    if (isMobile) {
+      let start_scroll = null
+      const onTouchStart = (event) => {
+        if (checkSwipeClass(event)) {
+          start_scroll = null
+        } else {
+          start_scroll = {
+            x: event.changedTouches[0].clientX,
+            y: event.changedTouches[0].clientY,
+          }
+        }
+      }
+      const onTouchMove = (event) => {
+        if (start_scroll && event.changedTouches?.length) {
+          if (Math.abs(start_scroll.y - event.changedTouches[0].clientY > 50)) {
+            start_scroll = null
+            return
+          }
+          if (event.changedTouches[0].clientX - start_scroll.x > 100) {
+            bodyRef.current.style = "overflow: hidden;"
+            setOpen(true)
+          } else if (start_scroll.x - event.changedTouches[0].clientX > 100) {
+            bodyRef.current.style = "overflow: overlay;"
+            setOpen(false)
+          }
+        }
+      }
+
+      const root = rootRef.current
+      root.addEventListener("touchstart", onTouchStart)
+      root.addEventListener("touchmove", onTouchMove)
+
+      return () => {
+        root.removeEventListener("touchstart", onTouchStart)
+        root.removeEventListener("touchmove", onTouchMove)
+      }
+    }
+  }, [isMobile])
+
+  return { open, closeMenu }
+}
